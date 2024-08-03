33ed7f3297fe48222ab2a00c36cd6f2f5d5badb3
sonson <sonson@example.com>
2024-08-03 16:44:38 +0000

    fix

--- solution/frontend/src/App.js
+++ solution/frontend/src/App.js
@@ -3,6 +3,8 @@ import MovieList from './components/MovieList';
 import MovieDetails from './components/MovieDetails';
 import './App.css';
 
+
+
 export default function App() {
   const [selectedMovie, setSelectedMovie] = useState(null);
 
